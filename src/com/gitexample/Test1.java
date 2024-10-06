229257a637a958637defecf318fa1fe769104b42
sai <saisharandurbakula@gmail.com>
2024-10-06 17:44:50 +0530

    This is my first commit!!

---
+++ src/com/gitexample/Test1.java
@@ -0,0 +1,20 @@
+package com.gitexample;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes") {
+			
+			System.out.println("I Am Hungry");
+			
+		}
+		else {
+			System.out.println("I Am Not Hungry");
+		}
+
+	}
+
+}
